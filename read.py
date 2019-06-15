a8f67a2eed08aee5e9521d4c9848f49c82618b62
Moon Chiang <moonlove8310@gmail.com>
2019-06-15 20:37:55 +0800

    calc average

--- read.py
+++ read.py
@@ -6,4 +6,10 @@ with open('reviews.txt', 'r') as f:
         count += 1
         if count % 10000 == 0:
             print(len(data))
-print(len(data))
+print('there are', len(data), 'data')
+
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+
+print('the average lenght of the messages are', sum_len/len(data))
